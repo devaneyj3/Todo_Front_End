6ec8228f6802a15af3750792642dd0946fffd1d5
Jordan <jordandevaney28@gmail.com>
2020-11-19 14:09:31 -0500

    update axios instance

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 export const Axios = () => {
+    let environment =
+        process.env.NODE_ENV === "production"
+            ? "https://https://todo56.herokuapp.com/api/"
+            : "http://localhost:3000/api/";
     return axios.create({
-        baseURL: "http://localhost:3000/api/",
+        baseURL: environment,
     });
 };
