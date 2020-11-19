a954b1fe973b858fa2786019c2d318656dfff9c2
Jordan <jordandevaney28@gmail.com>
2020-11-19 14:23:59 -0500

    fix another typo in axios url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const Axios = () => {
     let environment =
         process.env.NODE_ENV === "production"
-            ? "https://todo56.herokuapp.com/api/todos"
+            ? "https://todo56.herokuapp.com/api/"
             : "http://localhost:3000/api/";
     return axios.create({
         baseURL: environment,
